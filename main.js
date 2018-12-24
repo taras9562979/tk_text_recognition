0717e34f9bcba4491dc4f0fe762b312ede956de9
Taras Kiritchenko <taras.kiritchenko@gmail.com>
2018-12-24 14:39:59 +0200

    added -> inserting Nodes

--- main.js
+++ main.js
@@ -22,8 +22,17 @@ function delete_btn_Clicked(event){
     mode = 'Delete Mode';
     console.log(mode);
 }
+function canvas_left_Clicked(event){
+    var coordinates = [event.offsetX,event.offsetY];
+    var width = 60;
+    var height = 30;
+    ctx.fillStyle = 'wheat';
+    ctx.fillRect(event.offsetX-width/2,event.offsetY-height/2,width,height);
+    console.log(coordinates);
+}
 
 insert_btn.addEventListener('click',insert_btn_Clicked);
 move_btn.addEventListener('click',move_btn_Clicked);
 connect_btn.addEventListener('click',connect_btn_Clicked);
 delete_btn.addEventListener('click',delete_btn_Clicked);
+canvas.addEventListener('click',canvas_left_Clicked);
